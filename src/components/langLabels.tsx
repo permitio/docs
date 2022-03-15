4ab3fb5b64d5b4942f0b8725b221ac4880459b65
orweis <orweis@gmail.com>
2022-03-15 21:47:17 +0200

    support dark mode for language labels

--- src/components/langLabels.tsx
+++ src/components/langLabels.tsx
@@ -2,24 +2,44 @@ import React from "react";
 
 export const CSharpLabel = () => (
   <span>
-    <img src={"/img/languages/csharp.svg"} style={{ width: 22 }} /> .Net
+    <img
+      className="langLabelImage"
+      src={"/img/languages/csharp.svg"}
+      style={{ width: 22 }}
+    />{" "}
+    .Net
   </span>
 );
 
 export const PythonLabel = () => (
   <span>
-    <img src={"/img/languages/python.svg"} style={{ width: 22 }} /> Python
+    <img
+      className="langLabelImage"
+      src={"/img/languages/python.svg"}
+      style={{ width: 22 }}
+    />{" "}
+    Python
   </span>
 );
 
 export const JavaLabel = () => (
   <span>
-    <img src={"/img/languages/Java.svg"} style={{ width: 22 }} /> Java
+    <img
+      className="langLabelImage"
+      src={"/img/languages/Java.svg"}
+      style={{ width: 22 }}
+    />{" "}
+    Java
   </span>
 );
 
 export const NodeJSLabel = () => (
   <span>
-    <img src={"/img/languages/javascript.svg"} style={{ width: 22 }} /> NodeJS
+    <img
+      className="langLabelImage"
+      src={"/img/languages/javascript.svg"}
+      style={{ width: 22 }}
+    />{" "}
+    NodeJS
   </span>
 );
