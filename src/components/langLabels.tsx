a411a0665c7a1b6e6525dc7107392d8efff8b1e4
orweis <orweis@gmail.com>
2022-03-15 21:15:23 +0200

    tabs

--- src/components/langLabels.tsx
+++ src/components/langLabels.tsx
@@ -2,24 +2,24 @@ import React from "react";
 
 export const CSharpLabel = () => (
   <span>
-    <img src={"/img/languages/csharp.svg"} style={{ width: 32 }} /> .Net
+    <img src={"/img/languages/csharp.svg"} style={{ width: 22 }} /> .Net
   </span>
 );
 
 export const PythonLabel = () => (
   <span>
-    <img src={"/img/languages/python.svg"} style={{ width: 32 }} /> Python
+    <img src={"/img/languages/python.svg"} style={{ width: 22 }} /> Python
   </span>
 );
 
 export const JavaLabel = () => (
   <span>
-    <img src={"/img/languages/Java.svg"} style={{ width: 32 }} /> Java
+    <img src={"/img/languages/Java.svg"} style={{ width: 22 }} /> Java
   </span>
 );
 
 export const NodeJSLabel = () => (
   <span>
-    <img src={"/img/languages/javascript.svg"} style={{ width: 32 }} /> NodeJS
+    <img src={"/img/languages/javascript.svg"} style={{ width: 22 }} /> NodeJS
   </span>
 );
