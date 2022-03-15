63515326c184085dce8ffd2039d65be9a9692501
orweis <orweis@gmail.com>
2022-03-15 20:50:06 +0200

    fix tabs and some typos

---
+++ src/components/langLabels.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const CSharpLabel = () => (
+  <span>
+    <img src={"/img/languages/csharp.svg"} style={{ width: 32 }} /> .Net
+  </span>
+);
+
+export const PythonLabel = () => (
+  <span>
+    <img src={"/img/languages/python.svg"} style={{ width: 32 }} /> Python
+  </span>
+);
+
+export const JavaLabel = () => (
+  <span>
+    <img src={"/img/languages/Java.svg"} style={{ width: 32 }} /> Java
+  </span>
+);
+
+export const NodeJSLabel = () => (
+  <span>
+    <img src={"/img/languages/javascript.svg"} style={{ width: 32 }} /> NodeJS
+  </span>
+);
