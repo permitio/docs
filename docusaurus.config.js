9b1a8766ff1e4b64e53b7758a8bf50099976f42c
Filip Grebowski <filip@permit.io>
2023-01-24 16:47:18 +0000

    Added videos and small fix to theming

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -151,7 +151,7 @@ const config = {
 			},
 			colorMode: {
 				defaultMode: "light",
-				disableSwitch: true,
+				disableSwitch: false,
 				respectPrefersColorScheme: false,
 			},
 			//search configuration
