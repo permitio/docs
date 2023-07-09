dd1437ab98310bfb7d56244516adb81e384d3261
Filip Grebowski <filip@permit.io>
2023-07-10 00:51:40 +0100

    Sizzled and ejected the TOC and EditIcon to create a Dynamic 'Edit this Page' button + added custom overide styling to the TOC

---
+++ src/theme/SkipToContent/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SkipToContent from "@theme-original/SkipToContent";
+
+// Top Navigation
+
+export default function SkipToContentWrapper(props) {
+	return (
+		<>
+			<SkipToContent {...props} />
+		</>
+	);
+}
---
+++ src/theme/TOC/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import OriginalTOC from "@theme-original/TOC";
+import EditThisPage from "@theme/EditThisPage";
+
+export default function TOC({ toc, editUrl, ...props }) {
+	const URL_BASE_PATH = "https://github.com/permitio/docs/tree/master/docs";
+	console.log("EDIT URL: ", window.location.pathname);
+
+	return (
+		<div className="toc-wrapper">
+			<h2>Contents</h2>
+			<OriginalTOC toc={toc} {...props} />
+			<EditThisPage
+				editUrl={URL_BASE_PATH + window.location.pathname + ".mdx"}
+			/>
+		</div>
+	);
+}
