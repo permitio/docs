de2d0f2cc89b8ace0372e2ee17b07bb60f39dace
Filip Grebowski <filip@permit.io>
2023-07-10 09:08:51 +0100

    Added material UI and added some styling to TOC

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import OriginalTOC from "@theme-original/TOC";
 import EditThisPage from "@theme/EditThisPage";
+import Divider from "@mui/material/Divider";
 
 export default function TOC({ toc, editUrl, ...props }) {
 	const URL_BASE_PATH = "https://github.com/permitio/docs/tree/master/docs";
@@ -9,6 +10,7 @@ export default function TOC({ toc, editUrl, ...props }) {
 		<div className="toc-wrapper">
 			<h2>Contents</h2>
 			<OriginalTOC toc={toc} {...props} />
+			{/* <Divider className="divider" /> */}
 			<EditThisPage
 				editUrl={URL_BASE_PATH + window.location.pathname + ".mdx"}
 			/>
