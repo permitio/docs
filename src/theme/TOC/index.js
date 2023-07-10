f965e48e8cb6ab2a759f6d823aac522330c33a4c
Filip Grebowski <filip@permit.io>
2023-07-10 11:13:58 +0100

    Added a tracking UTM to TOC link

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -5,15 +5,20 @@ import Divider from "@mui/material/Divider";
 
 export default function TOC({ toc, editUrl, ...props }) {
 	const URL_BASE_PATH = "https://github.com/permitio/docs/tree/master/docs";
+	const URL_TRACKING =
+		"?utm_source=docs&utm_medium=docs-toc&utm_campaign=edit-on-github&utm_id=permit-docs";
 
 	return (
 		<div className="toc-wrapper">
 			<h2>Contents</h2>
 			<OriginalTOC toc={toc} {...props} />
-			{/* <Divider className="divider" /> */}
-			<EditThisPage
-				editUrl={URL_BASE_PATH + window.location.pathname + ".mdx"}
-			/>
+			<span>
+				<EditThisPage
+					editUrl={
+						URL_BASE_PATH + window.location.pathname + ".mdx" + URL_TRACKING
+					}
+				/>
+			</span>
 		</div>
 	);
 }
