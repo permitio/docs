62c832f6aa3c465a8de9fc0e4519992cdcb7c04d
Filip Grebowski <filip@permit.io>
2023-07-10 12:06:05 +0100

    Added fix for homepage

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import OriginalTOC from "@theme-original/TOC";
 import EditThisPage from "@theme/EditThisPage";
 import Divider from "@mui/material/Divider";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 
 export default function TOC({ toc, editUrl, ...props }) {
+	const [pathEnding, setPathEnding] = useState(".mdx");
+
 	const URL_BASE_PATH = "https://github.com/permitio/docs/tree/master/docs";
 	const URL_TRACKING =
 		"?utm_source=docs&utm_medium=docs-toc&utm_campaign=edit-on-github&utm_id=permit-docs";
+
+	// TODO: Needs improvement.
+	// Avoids home.mdx files throwing a 404.
+	const getWindowLocationPathname = (pathname) => {
+		if (pathname === "/") {
+			return "home.mdx";
+		}
+
+		return ".mdx";
+	};
 
 	return (
 		<div className="toc-wrapper">
@@ -17,7 +29,10 @@ export default function TOC({ toc, editUrl, ...props }) {
 				{() => (
 					<EditThisPage
 						editUrl={
-							URL_BASE_PATH + window.location.pathname + ".mdx" + URL_TRACKING
+							URL_BASE_PATH +
+							window.location.pathname +
+							getWindowLocationPathname(window.location.pathname) +
+							URL_TRACKING
 						}
 					/>
 				)}
