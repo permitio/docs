cf1c3880a6e1cae2783f397299657a8c04087cd9
Filip Grebowski <filip@permit.io>
2023-07-21 18:03:00 +0100

    Updated top navbar

---
+++ src/theme/Icon/LightMode/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+export default function IconLightMode(props) {
+	return (
+		<svg
+			version="1.0"
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 75 75"
+			preserveAspectRatio="xMidYMid meet"
+			style={{ marginBottom: "4px" }}
+			className="dark-theme-toggle"
+		>
+			<g
+				transform="translate(0, 90) scale(0.1, -0.1)"
+				fill="#92A0B3"
+				stroke="none"
+			>
+				<path
+					d="M285 768 c-62 -32 -131 -104 -162 -172 -24 -52 -28 -74 -28 -146 0
+-72 4 -94 28 -146 36 -77 104 -145 182 -181 51 -24 73 -28 145 -28 72 0 94 4
+145 28 63 29 137 93 161 139 l14 26 -38 -10 c-56 -16 -152 -4 -218 26 -163 75
+-251 267 -197 429 11 31 17 57 14 57 -3 -1 -24 -10 -46 -22z"
+				/>
+			</g>
+		</svg>
+	);
+}
