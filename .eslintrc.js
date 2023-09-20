a3fa7176acfaeeef94c460a9c06b7e145c169a1b
Filip Grebowski <filip@permit.io>
2023-09-20 10:54:00 +0200

    Removed yarn lock file

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "spellcheck"],
+  rules: {
+    "react/prop-types": "off",
+    "no-unused-vars": ["error", { vars: "all", args: "after-used", ignoreRestSiblings: false }],
+    "spellcheck/spell-checker": [
+      "error",
+      {
+        comments: true,
+        strings: true,
+        identifiers: true,
+        templates: true,
+        lang: "en_US",
+        skipWords: [
+          "nodejs",
+          "faq",
+          "sdk",
+          "unassign",
+          "quickstart",
+          "Frontend",
+          "onboarding",
+          "Workspace",
+          "axios",
+          "dotnet",
+          "golang",
+          "erlang",
+          "kotlin",
+          "cpp",
+          "pho",
+          "integrations",
+          "href",
+          "dropdown",
+          "csharp",
+          "docusaurus",
+          "Fullstack",
+          "algolia",
+          "ecma",
+          "lang",
+        ],
+      },
+    ],
+    "prettier/prettier": [
+      "off",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
