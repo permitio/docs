54d45bf52ff15515db545311cf8e292824b7c881
Harel Panker <harelpanker@gmail.com>
2024-04-02 18:53:45 +0200

    try to solve error build

--- src/theme/Layout.js
+++ src/theme/Layout.js
@@ -1,13 +1,16 @@
 import React from "react";
 import OriginalLayout from "@theme-original/Layout";
 import Head from "@docusaurus/Head";
-import "remixicon/fonts/remixicon.css";
 
 export default function Layout(props) {
   return (
     <>
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css"
+          rel="stylesheet"
+        />
       </Head>
       <OriginalLayout {...props} />
     </>
