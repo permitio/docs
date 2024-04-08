73c1713f1a7cc1fcbc2b57f0d94b883a064f6453
Harel Panker <harelpanker@gmail.com>
2024-04-08 20:59:54 +0200

    finishing sidebar

--- sidebars.js
+++ sidebars.js
@@ -8,23 +8,46 @@ const sidebars = {
                 <div class='sidebar_top'>
                   <div class='sidebar_top_logo is-dark'>
                     <a target="_blank" rel="noopener noreferrer" href="https://permit.io">
-                      <img src="/logo/site-logos/marketing-dark.svg" />
+                      <img src="/logo/site-logos/marketing-dark.svg" alt='' />
                     </a>
                     <a href="/">
-                      <img src="/logo/site-logos/docs-dark.svg" />
+                      <img src="/logo/site-logos/docs-dark.svg" alt='' />
                     </a>
                   </div>
                   <div class='sidebar_top_logo is-light'>
                     <a target="_blank" rel="noopener noreferrer" href="https://permit.io">
-                      <img src="/logo/site-logos/marketing-light.svg" />
+                      <img src="/logo/site-logos/marketing-light.svg" alt='' />
                     </a>
                     <a href="/">
-                      <img src="/logo/site-logos/docs-light.svg" />
+                      <img src="/logo/site-logos/docs-light.svg" alt='' />
                     </a>
                   </div>
                 </div>
 
-                <div class='sidebar_bottom'></div>
+                <div class='sidebar_bottom'>
+                  <a href="/">
+                    <img src="/sidebar-top-icons/normal-icons/docs-home.svg" alt='' />
+                    <img src="/sidebar-top-icons/hover-icons/docs-home.svg" alt='' />
+                    Docs Home
+                  </a>
+                  <a target="_blank" rel="noopener noreferrer" href="https://www.permit.io/">
+                    <img src="/sidebar-top-icons/normal-icons/marketing-home.svg" alt='' />
+                    <img src="/sidebar-top-icons/hover-icons/marketing-home.svg" alt='' />
+                    Permit Homepage
+                  </a>
+                  <a target="_blank" rel="noopener noreferrer" href="https://permit-io.slack.com/join/shared_invite/zt-nz6yjgnp-RlP9rtOPwO0n0aH_vLbmBQ#/shared-invite/email">
+                    <img src="/sidebar-top-icons/normal-icons/slack.svg" alt='' />
+                    <img src="/sidebar-top-icons/hover-icons/slack.svg" alt='' />
+                    Slack Community
+                  </a>
+                  <a target="_blank" rel="noopener noreferrer" href="https://github.com/permitio">
+                    <img src="/sidebar-top-icons/normal-icons/github.svg" alt='' />
+                    <img src="/sidebar-top-icons/hover-icons/github.svg" alt='' />
+                    GitHub
+                  </a>
+                </div>
+
+                <hr />
               </div>`, // The HTML to be rendered
       defaultStyle: true, // Use the default menu item styling
     },
