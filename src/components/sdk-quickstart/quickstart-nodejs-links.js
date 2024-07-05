d887721c1406bad4298977f6a33b59e36284fcbe
Filip Grebowski <filip@permit.io>
2024-07-05 22:06:24 +0100

    Fixed links

--- src/components/sdk-quickstart/quickstart-nodejs-links.js
+++ src/components/sdk-quickstart/quickstart-nodejs-links.js
@@ -2,10 +2,10 @@
 import React from "react";
 
 export const firstRow = [
-  { type: "link", href: "category/resource", label: "Resource", icon: " " },
-  { type: "link", href: "category/role", label: "Role", icon: " " },
-  { type: "link", href: "category/tenant", label: "Tenant", icon: " " },
-  { type: "link", href: "category/user", label: "User", icon: " " },
+  { type: "link", href: "/category/resource", label: "Resource", icon: " " },
+  { type: "link", href: "/category/role", label: "Role", icon: " " },
+  { type: "link", href: "/category/tenant", label: "Tenant", icon: " " },
+  { type: "link", href: "/category/user", label: "User", icon: " " },
   { type: "link", href: "/sdk/nodejs/all-tenants", label: "All Tenants Examples", icon: " " },
   {
     type: "link",
