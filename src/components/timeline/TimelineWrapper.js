21f1a132727ea93ca0db01b691e8d00846ba02de
Filip Grebowski <filip@permit.io>
2025-01-03 10:48:02 +0000

    PR review changes

--- src/components/timeline/TimelineWrapper.js
+++ src/components/timeline/TimelineWrapper.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const TimelineWrapper = ({ children }) => {
   // Count the number of children and pass the index + 1 as a prop to each TimelineStep
-  const steps = React.Children.map(children, (child, index) =>
-    React.cloneElement(child, { stepNumber: index + 1 })
-  );
+  const steps = React.Children.toArray(children).map((child, index) => {
+    return React.isValidElement(child)
+      ? { ...child, props: { ...child.props, stepNumber: index + 1 } }
+      : child;
+  });
 
   return (
     <div
--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-const VideoPlayer = ({ src }) => {
+const VideoPlayer = ({
+  src,
+  autoPlay = true,
+  loop = true,
+  controls = true,
+  className = "w-full h-auto block",
+  ...props
+}) => {
   return (
-    <video controls autoPlay loop className="w-full h-auto block">
+    <video controls={controls} autoPlay={autoPlay} loop={loop} className={className} {...props}>
       <source src={src} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
