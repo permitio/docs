bce74d5c3b2bb8a1bf2aafbb7a9135c2d8d65528
Pradumna Saraf <pradumnasaraf@gmail.com>
2025-05-18 13:24:34 +0530

    Add Quick Start Guide section to sidebars

--- sidebars.js
+++ sidebars.js
@@ -571,6 +571,25 @@ const sidebars = {
     },
     {
       type: "category",
+      label: "Quick Start Guide",
+      collapsible: false,
+      collapsed: false,
+      className: "category-as-header",
+      items: [
+        "quick-start/fastapi",
+        "quick-start/django",
+        "quick-start/flask",
+        "quick-start/nextjs",
+        "quick-start/nest",
+        "quick-start/express",
+        "quick-start/gin",
+        "quick-start/spring-boot",
+        "quick-start/aspnet",
+        "quick-start/rails"
+      ]
+    },
+    {
+      type: "category",
       label: "Integrate with Authentication",
       collapsed: false,
       collapsible: false,
