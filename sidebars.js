6b0b6362277bb87e6eaa6fa2d2c690cc3901aa45
alon-boshi <alon@permit.io>
2026-02-01 15:22:13 +0200

    cr

--- sidebars.js
+++ sidebars.js
@@ -472,7 +472,7 @@ const sidebars = {
             "api/pdp-webhooks",
             {
               type: "doc",
-              id: "how-to/monitoring-pdps",
+              id: "how-to/monitoring-pdps/monitoring-pdps",
               label: "Monitoring PDPs",
             },
           ],
